9e8f2d2a2a015a2cdb5bd60d89e396183bb71370
Sahad2701 <skachhawala33@gmail.com>
2024-10-31 19:12:53 +0530

    update: remove icon when dragging and add animations

--- lib/main.dart
+++ lib/main.dart
@@ -61,10 +61,15 @@ class Dock<T extends Object> extends StatefulWidget {
   @override
   State<Dock<T>> createState() => _DockState<T>();
 }
+
 /// State of the [Dock] used to manipulate the [_items].
 class _DockState<T extends Object> extends State<Dock<T>> {
   /// The items in the dock, managed by a [ValueNotifier] to enable reactivity.
   late final ValueNotifier<List<T>> _itemsNotifier;
+
+  late final List<T> _initialOrder;
+
+  T? _draggedItem;
 
   /// Index of the item being dragged, or null if no item is being dragged.
   final ValueNotifier<int?> _draggingIndexNotifier = ValueNotifier<int?>(null);
@@ -72,23 +77,16 @@ class _DockState<T extends Object> extends State<Dock<T>> {
   /// Index where the item is being hovered.
   final ValueNotifier<int?> _hoverIndexNotifier = ValueNotifier<int?>(null);
 
-  /// Timer for controlling the delay for long-press actions.
-  Timer? _longPressTimer;
-
-  /// Long press duration before initiating a drag.
-  static const Duration _longPressDuration = Duration(milliseconds: 150);
-
   @override
   void initState() {
     super.initState();
-    /// Initializes the notifier with a copy of the items.
-    _itemsNotifier = ValueNotifier<List<T>>(widget.items.toList());
+    _initialOrder = List<T>.from(widget.items);
+    _itemsNotifier = ValueNotifier<List<T>>(List<T>.from(_initialOrder));
   }
 
   @override
   void dispose() {
     /// Cancel active timers and dispose of the notifiers to prevent data leaks.
-    _longPressTimer?.cancel();
     _itemsNotifier.dispose();
     _draggingIndexNotifier.dispose();
     _hoverIndexNotifier.dispose();
@@ -97,67 +95,84 @@ class _DockState<T extends Object> extends State<Dock<T>> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(4),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.black12,
-      ),
-      child: ValueListenableBuilder<List<T>>(
-        valueListenable: _itemsNotifier,
-        builder: (context, items, _) {
-          return Row(
-            mainAxisSize: MainAxisSize.min,
-            children: items.asMap().entries.map((entry) {
-              return DraggableItemWidget<T>(
-                item: entry.value,
-                index: entry.key,
-                builder: widget.builder,
-                hoverIndexNotifier: _hoverIndexNotifier,
-                draggingIndexNotifier: _draggingIndexNotifier,
-                onLongPress: _startLongPress,
-                onDragStarted: _onDragStarted,
-                onDragEnd: _resetDrag,
-                reorderItems: _reorderItems,
-                itemsNotifier: _itemsNotifier,
-              );
-            }).toList(),
-          );
-        },
+    return AnimatedSize(
+      duration: const Duration(milliseconds: 200),
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.black12,
+        ),
+        child: ValueListenableBuilder<List<T>>(
+          valueListenable: _itemsNotifier,
+          builder: (context, items, _) {
+            return Row(
+              mainAxisSize: MainAxisSize.min,
+              children: items.asMap().entries.map((entry) {
+                final item = entry.value;
+                final index = entry.key;
+
+                return DraggableItemWidget<T>(
+                  item: item,
+                  index: index,
+                  builder: widget.builder,
+                  hoverIndexNotifier: _hoverIndexNotifier,
+                  draggingIndexNotifier: _draggingIndexNotifier,
+                  onDragStarted: () => _onDragStarted(index),
+                  onDragEnd: _resetDrag,
+                  reorderItems: _reorderItems,
+                  itemsNotifier: _itemsNotifier,
+                );
+              }).toList(),
+            );
+          },
+        ),
       ),
     );
   }
 
   /// Called when a drag operation starts.
   void _onDragStarted(int index) {
-    _longPressTimer?.cancel();
-    _draggingIndexNotifier.value = index;
+    if (_draggingIndexNotifier.value == null) {
+      /// Ensures the item at the specified index is removed only once
+      _draggedItem = _itemsNotifier.value[index];
+      _itemsNotifier.value = List<T>.from(_itemsNotifier.value)
+        ..removeAt(index);
+      _draggingIndexNotifier.value = index;
+    }
   }
 
   /// Reorders items in the dock.
   void _reorderItems(int oldIndex, int newIndex) {
-    if (oldIndex == newIndex) return;
+    if (oldIndex == newIndex || _draggedItem == null) return;
+    if (_itemsNotifier.value.length < 5 && _itemsNotifier.value.length == 4) {
+      final updatedItems = List<T>.from(_itemsNotifier.value);
 
-    final updatedItems = List<T>.from(_itemsNotifier.value);
-    final item = updatedItems.removeAt(oldIndex);
-    updatedItems.insert(newIndex, item);
+      updatedItems.removeWhere((item) => item == _draggedItem);
 
-    _itemsNotifier.value = updatedItems;
-    _hoverIndexNotifier.value = newIndex;
+      updatedItems.insert(newIndex, _draggedItem!);
+
+      _initialOrder
+        ..clear()
+        ..addAll(updatedItems);
+
+      _itemsNotifier.value = updatedItems;
+      _hoverIndexNotifier.value = newIndex;
+    }
   }
 
   /// Resets the dragging state.
   void _resetDrag() {
-    _longPressTimer?.cancel();
+    if (_draggedItem != null && _itemsNotifier.value.length == 5) {
+      final currentItems = List<T>.from(_itemsNotifier.value);
+      if (!currentItems.contains(_draggedItem)) {
+        currentItems.insert(_draggingIndexNotifier.value!, _draggedItem!);
+      }
+      _itemsNotifier.value = currentItems;
+    }
+    _draggedItem = null;
     _draggingIndexNotifier.value = null;
     _hoverIndexNotifier.value = null;
-  }
-
-  /// Initiates a long press timer for the given index.
-  void _startLongPress(int index) {
-    _longPressTimer = Timer(_longPressDuration, () {
-      _draggingIndexNotifier.value = index;
-    });
   }
 }
 
@@ -171,7 +186,6 @@ class DraggableItemWidget<T extends Object> extends StatelessWidget {
     required this.builder,
     required this.hoverIndexNotifier,
     required this.draggingIndexNotifier,
-    required this.onLongPress,
     required this.onDragStarted,
     required this.onDragEnd,
     required this.reorderItems,
@@ -193,11 +207,8 @@ class DraggableItemWidget<T extends Object> extends StatelessWidget {
   /// Notifier for the index being dragged.
   final ValueNotifier<int?> draggingIndexNotifier;
 
-  /// Callback for long press actions.
-  final void Function(int) onLongPress;
-
-  /// Callback for when dragging starts.
-  final void Function(int) onDragStarted;
+  /// Callback for when dragging Starts.
+  final VoidCallback onDragStarted;
 
   /// Callback for when dragging ends.
   final VoidCallback onDragEnd;
@@ -226,11 +237,10 @@ class DraggableItemWidget<T extends Object> extends StatelessWidget {
           onLeave: (_) => _clearHover(),
           builder: (context, candidateData, rejectedData) {
             return GestureDetector(
-              onLongPress: () => onLongPress(index),
-              onLongPressUp: _cancelLongPress,
+              onLongPress: onDragStarted,
               child: LongPressDraggable<T>(
                 data: item,
-                onDragStarted: () => onDragStarted(index),
+                onDragStarted: onDragStarted,
                 onDragCompleted: onDragEnd,
                 onDraggableCanceled: (_, __) => onDragEnd(),
                 feedback: Opacity(
@@ -241,8 +251,12 @@ class DraggableItemWidget<T extends Object> extends StatelessWidget {
                   valueListenable: draggingIndexNotifier,
                   builder: (context, draggingIndex, child) {
                     return hoverIndex == index
-                        ? Opacity(opacity: 0.5, child: builder(item))
-                        : builder(item);
+                        ? const Offstage()
+                        : AnimatedOpacity(
+                            duration: const Duration(milliseconds: 300),
+                            opacity: draggingIndex == index ? 0.0 : 1.0,
+                            child: builder(item),
+                          );
                   },
                 ),
               ),
@@ -267,10 +281,5 @@ class DraggableItemWidget<T extends Object> extends StatelessWidget {
   /// Clears the hover state.
   void _clearHover() {
     hoverIndexNotifier.value = null;
-  }
-
-  /// Stops the draggable action.
-  void _cancelLongPress() {
-    onDragEnd();
   }
 }
